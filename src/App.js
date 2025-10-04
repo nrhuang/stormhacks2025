aba6e5277d041dc8985ce45a4b61c6764913c274
Jaclyn Sunardi <jsa411@sfu.ca>
2025-10-04 15:15:04 -0700

    Revert "added read aloud functionality"
    This reverts commit 9f1a8261a8a2fd8179c78ef2933580c5cf5b4092.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-// ...existing code...
 import React from 'react';
 import './App.css';
 import VideoSection from './components/VideoSection';
 import ChatSection from './components/ChatSection';
 
 function App() {
-  // Called after the AI response is added to the UI
-  async function handleAiResponse(responseText) {
-    if (window && typeof window.speakTextFromMarkdown === 'function') {
-      try {
-        window.speakTextFromMarkdown(responseText);
-      } catch (e) {
-        console.warn('speak failed', e);
-      }
-    }
-  }
-
   return (
     <div className="container">
       <VideoSection />
-      <ChatSection onAiResponse={handleAiResponse} />
+      <ChatSection />
     </div>
   );
 }
 
 export default App;
-// ...existing code...
